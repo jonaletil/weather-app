38b49ac28110676f9e1ea3a0fb9f92d093b45ab1
Josip <josip.naletilic@netformic.de>
2022-10-23 17:26:48 +0200

    add api test

---
+++ tests/api_testing.py
@@ -0,0 +1,20 @@
+import configparser
+from nose.tools import assert_true
+import requests
+
+# api key for weather api
+config = configparser.ConfigParser()
+config.read('../api_keys.cfg')
+api_key = config['OPENWEATHER']['API_KEY']
+
+# url data
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Tuebingen&&units=metric&appid='
+API = BASE_URL + api_key
+
+
+def test_request_response():
+    # Send a request to the API
+    response = requests.get(API)
+
+    # Confirm that the request completed
+    assert_true(response.ok)
